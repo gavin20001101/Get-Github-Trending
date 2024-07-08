f0fe0413f4a4912ec544d36aa31c6c7071d1256c
林沅 <gavin891101@yahoo.com.tw>
2024-07-08 20:49:54 +0800

    加入參考網頁

--- github_trending.py
+++ github_trending.py
@@ -5,11 +5,14 @@ from googletrans import Translator
 # GitHub趨勢頁面的URL
 url = "https://github.com/trending?since=daily&spoken_language_code="
 
+# https://steam.oxxostudio.tw/category/python/spider/beautiful-soup.html#a3
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc.zh/#find-all
 web = requests.get(url)                        # 取得網頁內容
 soup = BeautifulSoup(web.text, "html.parser")  # 轉換成標籤樹
 h2 = soup.find_all("h2","h3 lh-condensed") # 取得所有 h2 標籤
 p = soup.find_all("p","col-9 color-fg-muted my-1 pr-4")   # 取得所有 p 標籤
 
+# https://bbs.mnya.tw/d/8670-python-shi-fou-you-ke-yi-ying-fan-zhong-de-fan-yi-gong-ju
 translator = Translator()                      # 建立翻譯器
 
 for i in range(0, 14):
--- run.command
+++ run.command
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Get the curent directory
+# https://askubuntu.com/questions/993078/what-does-0-in-shell-scripts-do
 dir="${0%/*}"
 cd $dir
 
+# https://tomchen.me/2020/03/22/OS/%5BMac&Linux%5D%20bash%20shell%E7%AD%86%E8%A8%98-%20%E7%94%A8shell%20script%E5%9F%B7%E8%A1%8C%E4%BD%A0%E7%9A%84%20Python%E7%A8%8B%E5%BC%8F/
 # 設定檔案權限
 chmod u+x github_trending.py 
 
